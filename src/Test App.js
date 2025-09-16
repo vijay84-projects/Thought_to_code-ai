e06b35435e72b910d842e5666da96275b772990c
Telugu Vijay <luckyvvk99@gmail.com>
2025-09-16 20:08:27 +0530

    Test App.js

---
+++ src/Test App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      <h1>🚀 Thoughts-To-Code AI</h1>
+      <p>If you see this, React is working fine.</p>
+    </div>
+  );
+}
+
+export default App;
